8518c4d16f87fe7f6edbda152acee8f273ef3a26
Robertus Dwi Ari Utomo <robert.unix98@gmail.com>
2019-03-05 19:43:45 +0700

    First Commit

---
+++ MagangRAION/RaionMagang3/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [Range(-30,30)] public float speedScrollScale;
+
+    public static BackgroundScroller instance;
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+    }
+}
